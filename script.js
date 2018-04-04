393ceebdf59e64c9ca6185a122dfc23c2f955997
Lucy Wilson <lucyleleu@googlemail.com>
2018-04-04 19:19:31 +0000

    added disabled class

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-    
-    var cards = [1,1,2,2,3,3,4,4,5,5,6,6,];
+$(document).ready(function() {
+
+    var cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, ];
     cards = shuffle(cards);
-    
+
     //shuffle code from:
     //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array    
     function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
         while (0 !== currentIndex) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
@@ -17,51 +18,59 @@ $(document).ready(function(){
         return array;
     }
     //end
-    
+
     //assign shuffled cards to divs
     assignIndex();
     assignClass();
-    function assignIndex(){
-        $('.card').each(function(index){
-            $(this).attr('data-card-index', cards[index]);     
+
+    function assignIndex() {
+        $('.card').each(function(index) {
+            $(this).attr('data-card-index', cards[index]);
         });
     }
-    
-    function assignClass(){
-        $('.card').each(function(index){
-            $(this).addClass("color-" + cards[index]);     
+
+    function assignClass() {
+        $('.card').each(function(index) {
+            $(this).addClass("color-" + cards[index]);
         });
     }
-    
+
     //add event listeners to display cards on click and add selected class
-    
-    $('.card').on('click', function(){
-        $(this).addClass('selected')
+
+    $('.card').on('click', function() {
+        $(this).addClass('selected disabled')
+        
         checkMatch();
+        $('.disabled').off('click');
+        console.log("clicked");
     });
-    
-    
-    function checkMatch(){
-        if($('.selected').length == 2) {
-            if($('.selected').first().data('card-index') == $('.selected').last().data('card-index')){
+
+
+
+    function checkMatch() {
+        if ($('.selected').length == 2) {
+            if ($('.selected').first().data('card-index') == $('.selected').last().data('card-index')) {
                 console.log("match");
                 //remove class "notMatched"
-                $('.selected').each(function(){
-                    $(this).removeClass("notMatched");     
-            });
+                $('.selected').each(function() {
+                    $(this).removeClass("notMatched").addClass("disabled");
+                });
                 //disable cards so can't be clicked again
                 //animate cards to show match
                 //leave displayed
-                
-            } else {
-                console.log("no");
+
+            }
+            else {
+                console.log("no match");
+                $('.selected').removeClass('disabled');
                 //animate to show no match
                 //after a delay of 1 second turn both cards back over
             }
-            $('.card').each(function(){
-                $(this).removeClass("selected");     
-            });
+            
         }
     }
     
+    
+    
+        
 });
