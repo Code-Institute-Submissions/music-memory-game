1f4556f3ff9176e0e69be3cd42624632a7852f9b
Lucy Wilson <lucyleleu@googlemail.com>
2018-04-05 15:38:48 +0000

    animated match

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
     var cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, ];
     cards = shuffle(cards);
 
+    //assign shuffled cards to divs
+    assignIndex();
+    assignClass();
+
+    //add event listeners to display cards on click and add selected class
+    $('.card').on('click', function() {
+        $(this).addClass('selected disabled animated flipInY').removeClass("faceDown")
+        checkMatch();
+    });
+
+
     //shuffle code from:
     //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array    
     function shuffle(array) {
@@ -19,9 +30,6 @@ $(document).ready(function() {
     }
     //end
 
-    //assign shuffled cards to divs
-    assignIndex();
-    assignClass();
 
     function assignIndex() {
         $('.card').each(function(index) {
@@ -35,50 +43,28 @@ $(document).ready(function() {
         });
     }
 
-    //add event listeners to display cards on click and add selected class
-
-    $('.card').on('click', function() {
-        
-        $(this).addClass('selected disabled animated flipInY').removeClass("hidden")
-        checkMatch();
-        $('.disabled').off('click');
-    });
-
-
-
     function checkMatch() {
         if ($('.selected').length == 2) {
             if ($('.selected').first().data('card-index') == $('.selected').last().data('card-index')) {
-                console.log("match");
-                //remove class "notMatched"
-                $('.selected').each(function() {
-                    $(this).removeClass("notMatched selected");
-                });
-                //disable cards so can't be clicked again
+                setTimeout(function() {
+                    $('.selected').each(function() {
+                        $(this).removeClass("flipInY notMatched selected").addClass("tada");
+                    });
+                    console.log("match");
+                }, 700);
                 //animate cards to show match
                 //leave displayed
-
             }
             else {
-               
-                setTimeout(function(){
-                  $('.selected').each(function(){
-                      $(this).removeClass("flipInY").addClass("animated bounce");
-                  });
-                   console.log("no match");
-                }, 1000);
-                
-               
-                
-               
+                setTimeout(function() {
+                    $('.selected').each(function() {
+                        $(this).removeClass("flipInY disabled selected").addClass("faceDown");
+                    });
+                    console.log("no match");
+                }, 700);
                 //animate to show no match
                 //after a delay of 1 second turn both cards back over
             }
-            
         }
     }
-    
-    
-    
-        
 });
