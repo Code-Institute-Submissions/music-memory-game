0021ed798fbc51226d543b7f8e6713375243f69c
Lucy Wilson <lucyleleu@googlemail.com>
2018-04-15 14:44:44 +0000

    added win sound

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
-    
-    
-    
+
+
+
 
     var cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
     cards = shuffle(cards);
-    
+
     var bflat = new Audio();
-    bflat.src = "b-flat.mp3";
-    
+    bflat.src = "assets/sounds/b-flat.mp3";
+
     var c = new Audio();
-    c.src = "c.mp3";
-    
+    c.src = "assets/sounds/c.mp3";
+
     var d = new Audio();
-    d.src = "d.mp3";
-    
+    d.src = "assets/sounds/d.mp3";
+
     var e = new Audio();
-    e.src = "e.mp3";
-    
+    e.src = "assets/sounds/e.mp3";
+
     var f = new Audio();
-    f.src = "f.mp3";
-    
-    
+    f.src = "assets/sounds/f.mp3";
+
+    var g = new Audio();
+    g.src = "assets/sounds/g.mp3";
+
+    var win = new Audio();
+    win.src = "assets/sounds/win.mp3";
+
+
 
     //assign shuffled cards to divs
     assignIndex();
@@ -30,25 +36,32 @@ $(document).ready(function() {
     //add event listeners to display cards on click and add selected class
     $('.card').on('click', function() {
         $(this).addClass('flipped selected disabled');
-        
-        if ($(this).data('card-index') == "1"){
+
+        if ($(this).data('card-index') == "1") {
             bflat.play();
-        } else if ($(this).data('card-index') == "2"){
+        }
+        else if ($(this).data('card-index') == "2") {
             c.play();
-        } else if ($(this).data('card-index') == "3"){
+        }
+        else if ($(this).data('card-index') == "3") {
             d.play();
-        } else if ($(this).data('card-index') == "4"){
-            e.play(); 
-        } else if ($(this).data('card-index') == "5"){
+        }
+        else if ($(this).data('card-index') == "4") {
+            e.play();
+        }
+        else if ($(this).data('card-index') == "5") {
             f.play();
-        } else if ($(this).data('card-index') == "6"){
-            g.play();  
-        }    
-        
-       
-        
-        
+        }
+        else if ($(this).data('card-index') == "6") {
+            g.play();
+        }
+
+
+
+        console.log($('.disabled').length);
         checkMatch();
+        checkForWin();
+
     });
 
 
@@ -67,7 +80,7 @@ $(document).ready(function() {
         return array;
     }
     //end
-    
+
 
 
     function assignIndex() {
@@ -106,5 +119,13 @@ $(document).ready(function() {
                 //after a delay of 1 second turn both cards back over
             }
         }
+    }
+
+    function checkForWin() {
+        setTimeout(function() {
+            if ($('.disabled').length == 12) {
+                win.play();
+            }
+        }, 700);
     }
 });
